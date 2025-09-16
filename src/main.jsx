6e61c5ffe25f57faa7dfd8c76f2187a064f6a494
arunjo96 <arunkumarvadivel96@gmail.com>
2025-09-17 00:30:18 +0530

    backend api added

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
