e8baa433a47136f01d9f39cf1784d777bdd6ada5
arunjo96 <arunkumarvadivel96@gmail.com>
2025-09-17 14:19:59 +0530

    link changed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
